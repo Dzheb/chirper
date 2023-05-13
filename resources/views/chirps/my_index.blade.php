d231b8033fa75b2120ebd0a4bd8be0bca282402a
Dzheb <Dzheb2009@gmail.com>
2023-05-13 21:36:04 +0300

    create + edit

---
+++ resources/views/chirps/my_index.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .wellcome{
+font-size: 18px;
+border: solid 2px;
+box-shadow: 3px 5px #ccc;
+border-radius: 8px;
+position: absolute;
+top: 30px;
+background-color: #ccc;
+right: 30px;
+width: 200px;
+padding: 8px;
+        }
+    </style>
+</head>
+<body>
+    <div class = "wellcome">
+        <p>Привет!</p>
+
+    </div>
+
+</body>
+</html>
---
+++ resources/views/components/input-error.blade.php
@@ -0,0 +1,3 @@
+@foreach ($messages as $message)
+    <span class="input-error">{{ $message }}</span>
+@endforeach
