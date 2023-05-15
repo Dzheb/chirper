592190c8d6616afbd9ae077901c45c38480f4119
Dzheb <Dzheb2009@gmail.com>
2023-05-15 12:44:53 +0300

    notification added

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -5,12 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Events\ChirpCreated;
 
 class Chirp extends Model
 {
     use HasFactory;
     protected $fillable = [
         'message',
+    ];
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
     ];
     public function user(): BelongsTo
     {
--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -13,7 +13,7 @@
                 class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
             >{{ old('message') }}</textarea>
             <x-input-error :messages="$errors->get('message')" class="mt-2" />
-            <x-primary class="mt-4">{{ __('Chirp') }}</x-primary>
+            <x-button class="mt-4">{{ __('Chirp') }}</x-button>
         </form>
         <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
             @foreach ($chirps as $chirp)
